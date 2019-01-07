f5e2d65d09e820e3c3c18048fe98de41583cf6f6
admin <admin@LM.local>
2019-01-07 11:59:24 +0100

    affichage des todolists

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var express = require('express'),
     router = express.Router(),
     mongodb = require('mongodb'),
     MongoClient = mongodb.MongoClient,
-    ObjectId = mongodb.ObjectID,
+    // ObjectId = mongodb.ObjectID,
     url = "mongodb://localhost:27017/todolist";
 
 MongoClient.connect(url,
@@ -13,9 +13,16 @@ MongoClient.connect(url,
 
         let db = client.db('todolist');
 
+        router.use(function (req, res, next) {
+            res.setHeader('Content-Type', 'application/json');
+            next();
+        });
+
         router.get('/', function (req, res, next) {
-        db.collection('todolist').find({}).toArray(function (error, datas) {
-        return res.json(datas) 
-        })
-    });
+            db.collection('todos').find({}).toArray(function (error, datas) {
+                return res.json(datas) 
+            })
+        });
 });
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+  router = express.Router(),
+  mongodb = require('mongodb'),
+  MongoClient = mongodb.MongoClient,
+  // ObjectId = mongodb.ObjectID,
+  url = "mongodb://localhost:27017/todolist";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+MongoClient.connect(url,
+  { useNewUrlParser: true },
+
+  function (err, client) {
+    if (err) throw err;
+
+    let db = client.db('todolist');
+
+    // router.use(function (req, res, next) {
+    //   res.setHeader('Content-Type', 'application/json');
+    //   next();
+    // });
+    
+    /* GET home page. */
+    // router.get('/', function(req, res, next) {
+    //   res.render('index', { title: 'Express' });
+    // });
+
+    router.get('/', function (req, res, next) {
+      db.collection('todos').find({}).toArray(function (error, datas) {
+        res.render('index', { title: 'Todolist', todos: datas})
+      })
+    });
+
+  });
 
 module.exports = router;
