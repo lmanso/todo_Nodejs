d0fe82693fe27ba389e3e0e3c269e27b6b8a69ba
admin <admin@LM.local>
2019-01-08 16:24:58 +0100

    callback fonction et division du HTMl

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,55 +1,72 @@
 (function () {
     /* Déclare une fonction annonyme*/
-    var httpRequest;
-    /* On déclare httpRequest */
+    
+    var httpRequest = new XMLHttpRequest();
+    /* On crée une instance de l'objet XMLHTTPRequest */
 
 /* DELETE */
         document.querySelectorAll('.delete-button').forEach(function (element) {
         element.addEventListener('click', function () {
-            console.log(this.getAttribute('data-id'))
+            // console.log(this.getAttribute('data-id'));
             /* data-id à la valeur de l'id de mon todos */
-            makeRequest(this.getAttribute('data-id'), 'DELETE');
-        
+            makeRequest(this.getAttribute('data-id'), 'DELETE', null, function(res){
+                element.closest(".todo").remove();
+            });
         })
     });
 
 /* UPDATE */
     document.querySelectorAll('.done-button').forEach(function (element) {
         element.addEventListener('click', function () {
-            console.log(this.getAttribute('data-id'))
-            makeRequest(this.getAttribute('data-id'), 'PUT', {state:true});
+            var st = this.getAttribute('data-state') == "false";
+            makeRequest(this.getAttribute('data-id'), 'PUT', {state:st}, function (res) {
+                console.log('mis à jour');
+            });
         })
     });
 
 
-  function makeRequest(id, method, datas) {
-            httpRequest = new XMLHttpRequest();
-        /* On crée une instance de l'objet XMLHTTPRequest */
-
+    function makeRequest(id, method, datas, callback) {
         if (!httpRequest) {
             alert('Giving up :( Cannot create an XMLHTTP instance');
-        return false;
+            return false;
+        }
+      httpRequest.onreadystatechange = function (){
+          if (httpRequest.readyState === 4) {
+              /* Si la requête est terminée (4) */
+              if (httpRequest.status === 200) {
+                  /* Si la requête est un succes alors on affiche une alerte*/
+                  /*   alert(httpRequest.responseText); */
+                  callback(JSON.parse(httpRequest.responseText));
+              } else {
+                //   console.log(httpRequest.readyState);
+                  // console.log(httpRequest.status);
+                  alert('There was a problem with the request.');
+              }
+          }
       }
-      httpRequest.onreadystatechange = alertContents;
       httpRequest.open(method, `/api/${id}`, true);
       /*  On spécifie la méthode de transmission des données, 
       l'URL et le mode de transmission de la requête (true = asynchrone).
       ${var} Pour concatener une variable avec mon URI */
-      httpRequest.send(datas);
-      /* On execute la requête et on envois datas*/
-    }
+      httpRequest.setRequestHeader('Content-Type', 'application/json');
+      /* On spécifie qu'on veux un retour en JSON avec application/json */
+      httpRequest.send(datas ? JSON.stringify(datas) : null);
+      /* On execute la requête et on envois datas on spécifie qu'on l'envoi en string (stingify) et JSON*/
   
-  function alertContents() {
-    if (httpRequest.readyState === 4) {
-        /* Si la requête est terminée (4) */
-        if (httpRequest.status === 200) {
-        /* Si la requête est un succé alors on affiche une alerte*/
-            alert(httpRequest.responseText);
-        } else {
-            // console.log(httpRequest.readyState);
-            console.log(httpRequest.status);
-            alert('There was a problem with the request.');
-        }
-      }
-    }
-  })();
+    // function alertContents() {
+    //     if (httpRequest.readyState === 4) {
+    //         /* Si la requête est terminée (4) */
+    //         if (httpRequest.status === 200) {
+    //         /* Si la requête est un succé alors on affiche une alerte*/
+    //             alert(httpRequest.responseText);
+    //             callback(httpRequest.responseJSON);
+    //         } else {
+    //             console.log(httpRequest.readyState);
+    //             // console.log(httpRequest.status);
+    //             alert('There was a problem with the request.');
+    //         }
+    //     }
+    // }
+  }
+})();
--- routes/api.js
+++ routes/api.js
@@ -2,31 +2,38 @@ var express = require('express'),
     router = express.Router(),
     mongodb = require('mongodb'),
     MongoClient = mongodb.MongoClient,
-    // ObjectId = mongodb.ObjectID,
+    ObjectId = mongodb.ObjectID,
     url = "mongodb://localhost:27017/todolist";
 
-// MongoClient.connect(url,
-//     { useNewUrlParser: true },
+MongoClient.connect(url,
+    { useNewUrlParser: true },
+
+    function (err, client) {
+        if (err) throw err;
+
+        let db = client.db('todolist');
 
     router.use(function (req, res, next) {
         res.setHeader('Content-Type', 'application/json');
         next();
     });
         router.delete('/:id', function (req, res, next) {
-            db.collection('todos').deleteOne({ _id: ObjectId(req.params.id) }), 
+            db.collection('todos').deleteOne({ _id:ObjectId(req.params.id) }, 
             function (err, result) {
                 if(err) return next(err);
                 return res.json(result);
                 // db.close();
-            }
+            });
          });
 
         router.put('/:id', function (req, res, next) {
-            db.collection('todos').updateOne({ _id: ObjectId(req.params.id) }), {$set: req.body /*{state:true}*/},
+            db.collection('todos').updateOne({ _id:ObjectId(req.params.id) },
+             {$set: req.body /*{state:true}*/},
             function (err, result) {
                 if (err) return next(err);
                 return res.json(result);
-            }
+            });
         });
+});
 
 module.exports = router;
